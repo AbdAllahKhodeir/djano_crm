fbd5420e5bbc8871f3112c2238a3ba32ad2ff416
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-09 22:47:32 -0600

    added login and logout

--- website/views.py
+++ website/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if user logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "Please review your username and password!")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
