864bc5abff01b4a4b2ca7f6374ae8d9edaff9dd5
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-09 22:05:23 -0600

    configured mysql databse and migrated the project and app

--- crm/settings.py
+++ crm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'elederco',
         'USER': 'root',
-        'PASSWORD': '#',
+        'PASSWORD': 'Vandetta123$',
         'HOST': 'localhost',
         'PORT': '3306',
     }
